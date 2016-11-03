6713a226a76592810f7a00dc35bb1bbc3232423f
Vitali_Ihnatsenka <vitali.ihnatsenka.consultant@consumer.org>
2016-11-03 15:48:10 +0300

    generate content

--- src/main/java/by/epam/jmp/Main.java
+++ src/main/java/by/epam/jmp/Main.java
@@ -1,7 +1,6 @@
 package by.epam.jmp;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
@@ -9,6 +8,8 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -16,44 +17,55 @@ public class Main {
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase mongoDatabase = mongoClient.getDatabase("jmp");
 
-        //Create users
-        List<Document> users = new ArrayList<Document>();
-        for (int i = 0; i < 100; i++){
-            Document doc = new Document("name", "name" + i);
-            users.add(doc);
-        }
-        mongoDatabase.getCollection("users").insertMany(users);
-
+        mongoDatabase.getCollection("movies").drop();
+        mongoDatabase.getCollection("users").drop();
         //create movies
         List<Document> movies = new ArrayList<Document>();
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 10; i++) {
             Document doc = new Document("name", "name" + i).append("length", i * 3).append("date", getRandomDate());
             movies.add(doc);
         }
         mongoDatabase.getCollection("movies").insertMany(movies);
 
-        //add watched movies
-        MongoCursor<Document> cursor = mongoDatabase.getCollection("users").find().iterator();
-        try{
-            while (cursor.hasNext()){
-                cursor.next();
-            }
-        }finally {
-            cursor.close();
+        //Create users
+        List<Document> users = new ArrayList<Document>();
+        for (int i = 0; i < 10; i++){
+            Document doc = new Document("name", "name" + i);
+            doc.append("movies", getRandomDocumentSublist(movies, 3));
+            users.add(doc);
         }
 
         //add friends
-        for (int i = 0; i < 100; i++){
-
+        for(Document user: users) {
+            List<Document> friends = getRandomDocumentSublist(users, 4)
+                    .stream()
+                    .filter(friend->!friend.equals(user))
+                    .map(friend->{
+                        List<Document> messages = new ArrayList<>();
+                        for(int i = 0; i < 20; i++){
+                            messages.add(new Document("text", "text" + i).append("date", getRandomDate()));
+                        }
+                        friend.remove("friends");
+                        friend.remove("movies");
+                        return friend.append("messages", messages);
+                    })
+                    .collect(Collectors.toList());
+            user.append("friends", friends);
+            mongoDatabase.getCollection("users").insertOne(user);
         }
-
-        //add messages
-        for (int i = 0; i < 100; i++){
-
-        }
-
-
     }
+
+    public static List<Document> getRandomDocumentSublist(List<Document> superList, int sublistSize ){
+        if(sublistSize > superList.size() || sublistSize < 0){
+            throw new IllegalArgumentException("Incorrect sublist size");
+        }
+        List<Document> sublist = new ArrayList<>();
+        for(int i = 0; i < sublistSize; i++){
+            sublist.add(new Document(superList.get(new Random().nextInt(superList.size()))));
+        }
+        return sublist;
+    }
+
     public static Date getRandomDate(){
         long offset = Timestamp.valueOf("2016-01-01 00:00:00").getTime();
         long end = Timestamp.valueOf("2017-01-01 00:00:00").getTime();
